2635ac2332c5c135e4fa41b678aaef24c60378da
Garam Kim <codebykim@naver.com>
2021-02-20 15:05:37 +0900

    first commit

---
+++ CommonLibrary.h
@@ -0,0 +1,28 @@
+#pragma once
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define WIN32_LEAN_AND_MEAN
+
+#pragma comment (lib, "ws2_32.lib")
+#pragma comment (lib, "winmm.lib")
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <string_view>
+#include <memory>
+#include <functional>
+#include <chrono>
+#include <queue>
+
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <Windows.h>
+
+#define SERVER_PORT 25000
+#define SERVER_IP "127.0.0.1"
+#define BUFFER_SIZE 1024
+#define RING_BUFFER_SIZE 10000
+#define WINDOWCLASS_NAME L"TCPFighter"
+
+#define UM_SOCKET (WM_USER + 1)
